1fa56d37b9c84a8c23bae27a962539928d3b0a56
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-02-18 00:31:00 +0300

    Complited the task#1 and created a unit test

---
+++ lesson_1/Lesson_1/Lesson_1/SimpleNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    public class SimpleNumber
+    {
+        public bool CheckNumber(int numb)
+        {
+            int d = 0;
+            int i = 2;
+
+            while (i < numb)
+            {
+                if (numb % i == 0)
+                {
+                    d++;
+                }
+                i++;
+            }
+
+            if (d == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ lesson_1/Lesson_1/Lesson_1_Tests/Task_1_Test.cs
@@ -0,0 +1,73 @@
+﻿using Lesson_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Lesson_1_Tests
+{
+    [TestClass]
+    public class Task_1_Test
+    {
+        SimpleNumber simpleNumber;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            simpleNumber = new SimpleNumber();
+        }
+
+        [TestMethod]
+        public void CheckSimpleNumber_3_trueReturned()
+        {
+
+            int number = 3;
+            bool expected = true;
+
+            bool actual =  simpleNumber.CheckNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckSimpleNumber_4_falseReturned()
+        {
+
+            int number = 4;
+            bool expected = false;
+
+            bool actual = simpleNumber.CheckNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckSimpleNumber_7_trueReturned()
+        {
+
+            int number = 7;
+            bool expected = true;
+
+            bool actual = simpleNumber.CheckNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckSimpleNumber_28_falseReturned()
+        {
+
+            int number = 28;
+            bool expected = false;
+
+            bool actual = simpleNumber.CheckNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckSimpleNumber_67_trueReturned()
+        {
+
+            int number = 67;
+            bool expected = true;
+
+            bool actual = simpleNumber.CheckNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
