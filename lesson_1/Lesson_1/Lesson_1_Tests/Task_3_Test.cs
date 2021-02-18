a95529c68efa8e359f33e4875b315ed236719850
ВЛ Р <kolbt87@gmail.com>
2021-02-18 09:16:03 +0300

    Task #3 done and created Unit tests

---
+++ lesson_1/Lesson_1/Lesson_1_Tests/Task_3_Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task_3;
+
+namespace Lesson_1_Tests
+{
+    [TestClass]
+    public class Task_3_Test
+    {
+     
+        [TestMethod]
+        public void RecursionFibonacci_4_3Returned()
+        {
+            int nubmer = 4;
+            int expected = 3;
+
+            int actual = FibonacciNumber.RecursionFibonacci(nubmer);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RecursionFibonacci_10_55Returned()
+        {
+            int nubmer = 10;
+            int expected = 55;
+
+            int actual = FibonacciNumber.RecursionFibonacci(nubmer);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NoRecursionFibonacci_4_3Returned()
+        {
+            int nubmer = 4;
+            int expected = 3;
+
+            int actual = FibonacciNumber.NoRecursionFibonacci(nubmer);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NoRecursionFibonacci_10_55Returned()
+        {
+            int nubmer = 10;
+            int expected = 55;
+
+            int actual = FibonacciNumber.NoRecursionFibonacci(nubmer);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ lesson_1/Lesson_1/Task_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Требуется реализовать рекурсивную версию и версию без рекурсии (через цикл).
+Пример чисел Фибоначчи:
+F(0) = 0,
+F(1) = 1.
+Для остальных чисел:
+F(N) = F(N-2) + F(N-1).
+То есть для F(2) будет F(2) = F(0) + F(1) = 0 + 1 = 1. 
+F(3) будет F(3) = F(1) + F(2) = 1 + 1 = 2. 
+ */
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Введите число");
+                int n = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine($"Число: {FibonacciNumber.NoRecursionFibonacci(n)}");
+            }
+           
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
