850f87aa5916679796cd9b6f487c8dead5a75222
ВЛ Р <kolbt87@gmail.com>
2021-02-18 17:03:32 +0300

    done

--- lesson_1/Lesson_1/Task_2/Program.cs
+++ lesson_1/Lesson_1/Task_2/Program.cs
@@ -16,26 +16,26 @@ namespace Task_2
 
         public static int StrangeSum(int[] inputArray)
         {
-            int sum = 0;
-            for (int i = 0; i < inputArray.Length; i++)
+            int sum = 0; //O(1)
+            for (int i = 0; i < inputArray.Length; i++)//O(N)
             {
-                for (int j = 0; j < inputArray.Length; j++)
+                for (int j = 0; j < inputArray.Length; j++)//O(N)
                 {
-                    for (int k = 0; k < inputArray.Length; k++)
+                    for (int k = 0; k < inputArray.Length; k++)//O(N)
                     {
-                        int y = 0;
-
-                        if (j != 0)
+                        int y = 0;//O(1)
+                        if (j != 0)//O(1)
                         {
                             y = k / j;
                         }
 
-                        sum += inputArray[i] + i + k + j + y;
+                        sum += inputArray[i] + i + k + j + y;//O(1)
                     }
                 }
             }
 
-            return sum;
+            return sum;//O(1)
         }
     }
 }
+//Сложность алгоритма O(N*N*N) = O(N^3)
