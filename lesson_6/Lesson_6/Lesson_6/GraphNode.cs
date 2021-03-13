06ba124ef212f60381a681a76fbed6699a0028fa
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-03-13 23:32:54 +0300

    HW is done

---
+++ lesson_6/Lesson_6/Lesson_6/GraphNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_6
+{
+    public class GraphNode
+    {
+        public GraphNode(string name)
+        {
+            Name = name;
+            Edges = new List<Edge>();
+
+        }
+        public string Name { get; set; }
+        public List<Edge> Edges{ get; set; }
+
+    }
+
+    public class Edge
+    {
+        public Edge(GraphNode node, int weight)
+        {
+            Node = node;
+            Weight = weight;
+        }
+        public int Weight { get; set; }
+        public GraphNode Node { get; set; }
+    }
+
+}
---
+++ lesson_6/Lesson_6/Lesson_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphNode node_A = new GraphNode("A");
+            GraphNode node_B = new GraphNode("B");
+            GraphNode node_C = new GraphNode("C");
+            GraphNode node_D = new GraphNode("D");
+            GraphNode node_E = new GraphNode("E");
+            GraphNode node_F = new GraphNode("F");
+            GraphNode node_G = new GraphNode("G");
+
+            node_A.Edges.Add(new Edge(node_B, 1));
+            node_A.Edges.Add(new Edge(node_C, 2));
+
+            node_B.Edges.Add(new Edge(node_E, 4));
+            node_B.Edges.Add(new Edge(node_D, 2));
+
+            node_C.Edges.Add(new Edge(node_D, 3));
+
+            node_E.Edges.Add(new Edge(node_F, 3));
+
+            node_D.Edges.Add(new Edge(node_F, 5));
+            node_D.Edges.Add(new Edge(node_G, 1));
+
+            SearchGraphClass searchGraph = new SearchGraphClass();
+            LinkedList<GraphNode> path = searchGraph.DFS(node_A, node_G);
+
+            Console.WriteLine("--------------DFS---------------");
+            searchGraph.PrintPath(path);
+            Console.WriteLine("--------------BFS---------------");
+            searchGraph.BFS(node_A);
+
+        }
+    }
+}
